c2500ca999b87d022766672aa5b9121c9591150a
Maksim Ryzhikov <rv.maksim@gmail.com>
2022-03-30 12:19:43 +0200

    chore: add react and support markdown renderning

--- src/client/core.cljs
+++ src/client/core.cljs
@@ -1,9 +1,16 @@
-(ns client.core)
+(ns client.core
+  (:require
+   [reagent.dom :as rdom]
+   [markdown.core :refer [md->html]]))
+
+(defn html-elm [html]
+  [:div {:dangerouslySetInnerHTML {:__html html}}])
+
+(defn app-elm []
+  [:div [html-elm (md->html "Hello World!")]])
 
 (defn -main []
-  (set!
-    (.-innerText
-      (js/document.getElementById "app"))
-    "Hello World!"))
+  (let [app-node (js/document.getElementById "app")]
+    (rdom/render [app-elm] app-node)))
 
 (-main)
